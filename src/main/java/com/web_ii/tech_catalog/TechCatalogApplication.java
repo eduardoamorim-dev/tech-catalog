6a621fa5fbcf434e7077e1ed927c535303cbc480
eduardoamorim-dev <amorimm.dev@gmail.com>
2025-04-27 13:32:26 -0300

    feat: init project

---
+++ src/main/java/com/web_ii/tech_catalog/TechCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.web_ii.tech_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechCatalogApplication.class, args);
+	}
+
+}
