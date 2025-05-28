5e3f3bcdab914de16655bcc3edc76b1e329595a9
eduardoamorim-dev <amorimm.dev@gmail.com>
2025-05-28 20:30:53 -0300

    feat: Classe da entidade usuario e classe de persistencia de usuario

---
+++ src/main/java/com/web_ii/tech_catalog/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.web_ii.tech_catalog.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.web_ii.tech_catalog.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	Optional<User> findUserByEmail(String email);
+}
