859cba9e3ad9d5fee901dba7eb7c66e4489a853b
Carlos Eduardo Amorim <86995782+eduardoamorim-dev@users.noreply.github.com>
2025-06-02 21:40:24 -0300

    Add files via upload

---
+++ src/main/java/com/web_ii/tech_catalog/service/impl/TechCatalogServiceImpl.java
@@ -0,0 +1,44 @@
+package com.web_ii.tech_catalog.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.web_ii.tech_catalog.models.TechCatalog;
+import com.web_ii.tech_catalog.repository.TechCatalogRepository;
+import com.web_ii.tech_catalog.service.TechCatalogService;
+import com.web_ii.tech_catalog.service.TechCatalogService;
+
+    @Service
+    public class TechCatalogServiceImpl implements TechCatalogService {
+    
+        @Autowired
+        private TechCatalogRepository techCatalogRepository;
+    
+        @Override
+        public List <TechCatalog> getAllTechCatalog(){
+            return techCatalogRepository.findAll();
+        }
+    
+        @Override
+        public void saveTechCatalog(TechCatalog techCatalog){
+            this.techCatalogRepository.save(techCatalog);
+        }
+    
+        @Override
+        public TechCatalog getTechCatalogById(long id) {
+            Optional < TechCatalog > optional = techCatalogRepository.findById(id);
+            if (optional.isPresent()) {
+                return optional.get();
+            } else {
+                throw new RuntimeException("Tech Catalog not found with id: " + id);
+            }
+        }
+    
+        @Override
+        public void deleteTechCatalogById(long id) {
+            this.techCatalogRepository.deleteById(id);
+        }
+    
+    }
