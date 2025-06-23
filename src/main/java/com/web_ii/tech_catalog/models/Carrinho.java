feb363dd6266ccbdc93b76dc52f7161370414712
natalete <natsalete14@gmail.com>
2025-06-22 21:55:35 -0300

    feat: listagem de produtos

---
+++ src/main/java/com/web_ii/tech_catalog/models/Carrinho.java
@@ -0,0 +1,24 @@
+package com.web_ii.tech_catalog.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "eletronicos")
+public class Carrinho {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_produto", nullable = false)
+    private TechCatalog produto;
+    
+    
+}
